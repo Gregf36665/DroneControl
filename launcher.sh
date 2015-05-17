c8bb89e101278f810921f0bae7cfda7111d585d3
Gregf36665 <Flynng@lafayette.edu>
2015-05-17 17:33:27 -0400

    Added in more useful logging features (parenthesis around date and time stamp)

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,14 @@ ffplay tcp://192.168.1.1:5555  >& /dev/null &
 
 # resize terminal window
 resize -s 50 180 >&/dev/null
+
+# Print the date and time to the log file
+echo "`date +"%y-%m-%d %T"`{" >> log.txt #} to prevent vim folding
+
 #start up control
-node control.js -d 2> log.txt
+node control.js -d 2>> log.txt
 
 #close ffplay when done
 pkill -x ffplay
+
+echo "}" >> log.txt # close the log
